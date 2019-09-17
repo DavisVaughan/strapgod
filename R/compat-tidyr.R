cf5f4a4386edcee9a496a074e56c335d68f85325
DavisVaughan <davis@rstudio.com>
2019-09-17 06:37:55 -0400

    Use `deprecated()` for `.key`

--- R/compat-tidyr.R
+++ R/compat-tidyr.R
@@ -2,7 +2,8 @@
 # tidyr support
 
 #' @importFrom tidyr nest
+#' @importFrom lifecycle deprecated
 #' @export
-nest.resampled_df <- function(.data, ..., .key = "DEPRECATED") {
+nest.resampled_df <- function(.data, ..., .key = deprecated()) {
   tidyr::nest(collect(.data), ..., .key = .key)
 }
